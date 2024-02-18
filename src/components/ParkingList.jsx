27e256f5618e1095c2bf3e76507007fb86725182
D3VASTAT3 <moblieevils@gmail.com>
2024-02-19 05:49:53 +0800

    updated parking list design

--- src/components/ParkingList.jsx
+++ src/components/ParkingList.jsx
@@ -4,6 +4,8 @@ import {useEffect, useState} from "react";
 import Layout from "./layout/Layout";
 import {useNavigate} from "react-router-dom";
 
+
+
 const ParkingList = () => {
     const navigate = useNavigate()
     const [parkingList,setParkingList] = useState([])
@@ -34,25 +36,29 @@ const ParkingList = () => {
                         >Explore</button>
                     </div>
                 </div>
-                <hr className="m-10"/>
+                <hr className="m-5 border-slate-500"/>
+                <h1 className="text-black-500 text-4xl text-center font-sans font-bold mb-8">Parking Lots Near You</h1>
                 <ul role="list" className="grid grid-cols-1 md:grid-cols-3 2xl:grid-cols-5 gap-4 pr-10 pl-10">
                     {parkingList.map((parking)=>{
                         return (
-                            <li key={parking.ParkingID} className="border-solid border border-sky-500 p-4 rounded-md">
+                            <li key={parking.ParkingID} className="bg-gray-950 border-solid border p-4 rounded-md">
                                 <div >
-                                    <div className="block overflow-ellipsis truncate">
-                                        <p className="overflow-hidden text-ellipsis">{parking.ParkingID}</p>
-                                        <p className="overflow-hidden text-ellipsis">{parking.ParkingName}</p>
-                                        <p className="overflow-hidden text-ellipsis">{new Date(parking.created_at).toLocaleString()}</p>
-                                        <p className="overflow-hidden text-ellipsis">{parking.ParkingStatus ? "Available" : "Unavailable"}</p>
-                                        <p className="overflow-hidden text-ellipsis">{parking.FourWheelsStatus?`Four Wheels: ${parking.FourWheelsPrice  === null ? '-----' : parking.FourWheelsPrice} ` : ""}</p>
-                                        <p className="overflow-hidden text-ellipsis">{parking.TwoWheelsStatus?`Two Wheels: ${parking.TwoWheelsPrice === null ? '-----' : parking.TwoWheelsPrice}` : ""}</p>
+                                    <div className="flex-col block overflow-ellipsis truncate">
+                                        <p className="overflow-hidden text-ellipsis">{/*parking.ParkingID*/}</p>
+                                        <p className="overflow-hidden text-ellipsis text-2xl text-slate-200">{parking.ParkingName}</p>
+                                        <p className="overflow-hidden text-ellipsis text-slate-200">{new Date(parking.created_at).toLocaleString()}</p>
+                                        <p className="overflow-hidden text-ellipsis text-slate-200">{parking.ParkingStatus ? "Available" : "Unavailable"}</p>
+                                        <p className="overflow-hidden text-ellipsis text-slate-200">{parking.FourWheelsStatus?`Four Wheels: ${parking.FourWheelsPrice  === null ? '-----' : parking.FourWheelsPrice} ` : ""}</p>
+                                        <p className="overflow-hidden text-ellipsis text-slate-200">{parking.TwoWheelsStatus?`Two Wheels: ${parking.TwoWheelsPrice === null ? '-----' : parking.TwoWheelsPrice}` : ""}</p>
 
                                     </div>
-                                    <button
-                                        type={"button"}
-                                        className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                                        onClick={()=>navigate(`/myparking/${parking.ParkingID}`)}>Details</button>
+                                        <div className="flex justify-end">
+                                            <button
+                                            type={"button"}
+                                            
+                                            className="px-8 py-8 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
+                                            onClick={()=>navigate(`/myparking/${parking.ParkingID}`)}>Navigate</button>
+                                    </div>
                                 </div>
                             </li>
                         )

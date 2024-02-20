ee0b53066338c9eeddb4129e09eaf964e166f4e4
Toni Inot <tonigwapo@gmail.com>
2024-02-20 15:52:47 +0800

    Adding directions

--- src/components/map/MapDisplay.jsx
+++ src/components/map/MapDisplay.jsx
@@ -1,5 +1,7 @@
 import {APIProvider, AdvancedMarker, InfoWindow, Map,Pin, Marker, useAdvancedMarkerRef, useMarkerRef} from "@vis.gl/react-google-maps";
 import {useEffect, useState} from "react";
+import Directions from "./Directions";
+import {toast} from "react-toastify";
 
 const MapDisplay = ({data = [],page,markedLocation}) => {
     const position = {lat: 10.324444518537874, lng:123.95277453359705}
@@ -8,13 +10,34 @@ const MapDisplay = ({data = [],page,markedLocation}) => {
     const [openInfoWindow, setOpenInfoWindow] = useState(false)
     const [markerRef, useMarker] = useAdvancedMarkerRef()
     const [selectedMarker, setSelectedMarker] = useState(null)
- 
+
+    const [directions,setDirections] = useState({})
+    const [doDirections,setDoDirections] = useState(false)
+    const [origin,setOrigin] = useState('')
+
     useEffect(()=>{
         if(page==="CreateParking"){
             markedLocation({lat:marker.lat,lng:marker.lng})
 
         }
     },[marker])
+
+    useEffect(()=>{
+       if(navigator.geolocation) {
+           navigator.geolocation.getCurrentPosition(pos=>{
+               const lat = pos.coords.latitude
+               const lng = pos.coords.longitude
+
+               setOrigin(`${lat}, ${lng}`)
+           },err=>{
+               setOrigin('10.32546837125536, 123.95334301842492')
+               console.log(err)
+               toast.warning('Location is turned off.')
+           })
+       }else {
+           setOrigin('10.32546837125536, 123.95334301842492')
+       }
+    },[])
     
     if(page==="ParkingPage"){
         return (
@@ -31,11 +54,8 @@ const MapDisplay = ({data = [],page,markedLocation}) => {
                                     <AdvancedMarker 
                                         ref={markerRef}
                                         onClick={(e)=>{
-                                            console.log(e);
-
                                             setOpenInfoWindow(true);
-                                            setSelectedMarker(parking.ParkingID)
-                                            console.log(selectedMarker)}
+                                            setSelectedMarker(parking.ParkingID)}
                                         } 
                                         position={{lat:parseFloat(parking.Lat),lng:parseFloat(parking.Lng)}}>
                                     <Pin
@@ -48,7 +68,11 @@ const MapDisplay = ({data = [],page,markedLocation}) => {
                                             <div key={parking.ParkingID}>
                                                 <InfoWindow
                                                     position={{lat:parseFloat(parking.Lat),lng:parseFloat(parking.Lng)}}
-                                                    onCloseClick={() => setOpenInfoWindow(false)}>
+                                                    onCloseClick={() => {
+                                                        setOpenInfoWindow(false)
+                                                        setDoDirections(false)
+                                                        setDirections('')
+                                                    }}>
                                                     <div className="p-2 relative">
                                                         <div className="relative">
                                                             <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hautetime.com%2Fwp-content%2Fuploads%2F2015%2F08%2FPlace_Vendome_Paris_20_April_2011.jpg&f=1&nofb=1&ipt=18bea3a33b7591e2d27ec2e34c6089aaea807b81e074279344c21f863ab16741&ipo=images" className="max-h-52 rounded-md"></img>
@@ -57,7 +81,16 @@ const MapDisplay = ({data = [],page,markedLocation}) => {
                                                             </div>
                                                         </div>
                                                         <div className="mt-5">
-                                                            <p>asdagagad</p>
+                                                            <button
+                                                                className={"rounded-md bg-amber-600 p-3 hover:bg-amber-500"}
+                                                                onClick={()=>{
+                                                                    setDirections({
+                                                                        origin,
+                                                                        destination: `${parking.Lat}, ${parking.Lng}`
+                                                                    })
+                                                                    setDoDirections(true)
+                                                                }}
+                                                            >Directions</button>
                                                         </div>
                                                     
                                                     </div>
@@ -69,6 +102,7 @@ const MapDisplay = ({data = [],page,markedLocation}) => {
                                 </div>
                             )
                         })}
+                        {doDirections && <Directions directions={directions}/>}
                     </Map>
                 </div>
 

7bc1df9a211cf8aefb082f7e40acac5fad2fd65f
toni225 <73512652+toni225@users.noreply.github.com>
2024-05-11 13:12:05 +0800

    Update user.dao.js

--- src/models/persistence/user.dao.js
+++ src/models/persistence/user.dao.js
@@ -16,7 +16,6 @@ const getAllParking = async () => {
     const response = await supabase
         .from('ParkingDetails')
         .select('*,username (*)')
-        .order('ParkingName',{ascending:true})
         .order('ParkingStatus',{ascending:false})
 
     return response
